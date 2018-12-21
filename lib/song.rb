681ea9c90e6910107dc28dde2096657d2519ac6b
NaKia Whitby <whitby.nakia@gmail.com>
2018-12-21 23:32:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,22 @@
 class Song
-  attr_accessor :name, :artist
 
+  attr_accessor :artist, :name
   @@all = []
 
-  def initialize(name, artist=nil)
+  def initialize(name)
     @name = name
-    @artist = artist
     @@all << self
-  end
-
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
   end
 
   def self.all
     @@all
   end
+
+  def artist_name
+     if self.artist
+      self.artist.name
+    else
+      nil
+    end
+
 end
